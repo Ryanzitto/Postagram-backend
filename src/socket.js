a74e8280653d1fcbcd146dc69395634a806ca048
Ryan Henrique <ryanhardflip@gmail.com>
2024-03-20 20:01:38 -0300

    feat

--- src/socket.js
+++ src/socket.js
@@ -17,15 +17,25 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   socket.on("userConnected", (username) => {
+    let joinMessage = {
+      text: `${username} joined the chat.`,
+      type: "systemMessage",
+    };
     console.log("Usuário conectado:", username);
     connectedUsers.push(username);
     io.emit("updateUsers", connectedUsers);
+    io.emit("message", joinMessage);
   });
 
   socket.on("userDisconnected", (username) => {
+    let leftMessage = {
+      text: `${username} left the chat.`,
+      type: "systemMessage",
+    };
     console.log("Usuário desconectado:", username);
     connectedUsers = connectedUsers.filter((user) => user !== username);
     io.emit("updateUsers", connectedUsers);
+    io.emit("message", leftMessage);
   });
 
   const handleMessage = (data) => {
